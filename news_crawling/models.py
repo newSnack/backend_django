cc0dcec7c2128f4940e173d658cd858d61f97596
hdsung98 <hdsung98@naver.com>
2023-12-05 01:38:07 +0900

    news_crawling app updated - 뉴스 기사 저장 위해 model 및 serializers 생성 - views.py 함수명 업데이트, 네이버 api에 검색어를 콘솔입력으로 넘겨주던 방식을 request에서 받아 입력하는 방식으로 변경 - urls.py url 패턴 변경

--- news_crawling/models.py
+++ news_crawling/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    org_link = models.URLField()
+    link = models.URLField()
+    pub_date = models.DateTimeField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- news_crawling/views.py
+++ news_crawling/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import NewsSerializer
@@ -6,7 +7,8 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-def getNewsContent(url):
+
+def getPersonalNewsContent(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -20,7 +22,8 @@ def getNewsContent(url):
         print(f"Error while fetching news content: {e}")
         return None
 
-def getNaverSearch(node, srcText, start, display):
+
+def getPersonalNaverSearch(node, srcText, start, display):
     client_id = os.environ.get('NAVER_CLIENT_ID')
     client_secret = os.environ.get('NAVER_CLIENT_SECRET')
 
@@ -40,30 +43,33 @@ def getNaverSearch(node, srcText, start, display):
     else:
         print(f"Error: {response.status_code}")
         return None
-      
-class NewsListView(APIView):
-    def get(self, request, format=None):
-        srcText = request.query_params.get('query', None)
-        if srcText is None:
-            return Response({"message": "검색어를 입력하세요."})
 
-        jsonResponse = getNaverSearch('news', srcText, 1, 100)
-        if jsonResponse is None:
-            return Response({"message": "API 요청에 실패했습니다."})
+
+class PersonalNewsListView(APIView):
+    def get(self, request):
+        srcTexts = request.query_params.getlist('query')
+
+        if not srcTexts:
+            return Response({"message": "검색어가 없거나 비어있습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         jsonResult = []
-        for cnt, post in enumerate(jsonResponse.get('items', []), 1):
-            content = getNewsContent(post['link'])
-            postData = {
-                'cnt': cnt,
-                'title': post['title'],
-                'description': post['description'],
-                'org_link': post['originallink'],
-                'link': post['link'],
-                'pDate': post['pubDate'],
-                'content': content
-            }
-            jsonResult.append(postData)
+        for srcText in srcTexts:
+            jsonResponse = getPersonalNaverSearch('news', srcText, 1, 10)  # Fetch only 10 articles
+            if jsonResponse:
+                for cnt, post in enumerate(jsonResponse.get('items', []), 1):
+                    content = getPersonalNewsContent(post['link'])
+                    postData = {
+                        'cnt': cnt,
+                        'title': post['title'],
+                        'description': post['description'],
+                        'org_link': post['originallink'],
+                        'link': post['link'],
+                        'pDate': post['pubDate'],
+                        'content': content
+                    }
+                    jsonResult.append(postData)
+            else:
+                return Response({"message": f"API 요청에 실패했습니다: {srcText}"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
         serializer = NewsSerializer(jsonResult, many=True)
         return Response(serializer.data)
