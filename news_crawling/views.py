53d5feeeb11bdc9b7f7a6e601befc52b489159a5
hdsung98 <hdsung98@naver.com>
2023-12-05 01:42:57 +0900

    news_crawling app updated - beautifulsoup 크롤링 우회되고 있는 것 같아서, 본문 내용 크롤링하는 로직 주석처리해둠

--- news_crawling/views.py
+++ news_crawling/views.py
@@ -8,19 +8,19 @@ import os
 from bs4 import BeautifulSoup
 
 
-def getPersonalNewsContent(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            content_div = soup.find('div', id='ijam_content')
-            news_content = content_div.get_text(strip=True) if content_div else '내용이 없습니다.'
-            return news_content
-        else:
-            return None
-    except Exception as e:
-        print(f"Error while fetching news content: {e}")
-        return None
+# def getPersonalNewsContent(url):
+#     try:
+#         response = requests.get(url)
+#         if response.status_code == 200:
+#             soup = BeautifulSoup(response.content, 'html.parser')
+#             content_div = soup.find('div', id='ijam_content')
+#             news_content = content_div.get_text(strip=True) if content_div else '내용이 없습니다.'
+#             return news_content
+#         else:
+#             return None
+#     except Exception as e:
+#         print(f"Error while fetching news content: {e}")
+#         return None
 
 
 def getPersonalNaverSearch(node, srcText, start, display):
@@ -57,7 +57,7 @@ class PersonalNewsListView(APIView):
             jsonResponse = getPersonalNaverSearch('news', srcText, 1, 10)  # Fetch only 10 articles
             if jsonResponse:
                 for cnt, post in enumerate(jsonResponse.get('items', []), 1):
-                    content = getPersonalNewsContent(post['link'])
+                    # content = getPersonalNewsContent(post['link'])
                     postData = {
                         'cnt': cnt,
                         'title': post['title'],
@@ -65,7 +65,7 @@ class PersonalNewsListView(APIView):
                         'org_link': post['originallink'],
                         'link': post['link'],
                         'pDate': post['pubDate'],
-                        'content': content
+                        # 'content': content
                     }
                     jsonResult.append(postData)
             else:
