695665ffd1d0975ef1fedbd11ed3784a3facedce
kjyeon1101 <kjyeon1101@gmail.com>
2023-12-06 23:04:46 +0900

    like feed list

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,8 @@ router.register('list', UserViewSet) # 유저리스트 (테스트용)
 urlpatterns = [
     path("", include(router.urls)),
     path("initialize/", UserInitializeView.as_view()),
+    path("liked-feeds/private", UserLikedPrivateFeedsView.as_view()),
+    path("liked-feeds/public", UserLikedPublicFeedsView.as_view()),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/checkid/<str:id>/', IDCheckView.as_view())
