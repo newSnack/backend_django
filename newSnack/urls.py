95632013ba05d2d834be9809292731dd449fc41f
kjyeon1101 <kjyeon1101@gmail.com>
2023-12-06 18:16:29 +0900

    image & public authorize fix

--- newSnack/urls.py
+++ newSnack/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('api/feed/', include("feed.urls")),
     path('api/interest/', include("interest.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
