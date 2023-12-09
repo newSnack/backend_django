6a63afecc854f8be27814c72d41ddc13c49d5486
hdsung98 <hdsung98@naver.com>
2023-12-10 02:30:11 +0900

    유저의 interest_keyword 업데이트 로직 추가 - 좋아요,싫어요 및 그 undo 로직을 최적화하고, 클릭시 interest_keyword에 기사제목을 삽입 또는 삭제하도록 하는 로직을 추가했다

--- feed/views.py
+++ feed/views.py
@@ -52,25 +52,33 @@ class PrivateFeedLikeView(APIView):
 
     def post(self, request, pk):
         privateFeed = PrivateFeed.objects.get(pk=pk)
-        if (self.request.user == privateFeed.user):
-            likeOrDislike = request.data["likeOrDislike"]
-            originLike = privateFeed.likeOrDislike
-            if (originLike == 0):
-                privateFeed.likeOrDislike = likeOrDislike
-            elif (originLike == 1):
-                if (likeOrDislike == 1):  # 좋아요 취소
-                    privateFeed.likeOrDislike = 0
-                elif (likeOrDislike == -1):  # 싫어요
-                    privateFeed.likeOrDislike = -1
-            elif (originLike == -1):
-                if (likeOrDislike == 1):  # 좋아요
-                    privateFeed.likeOrDislike = 1
-                elif (likeOrDislike == -1):  # 싫어요 취소
-                    privateFeed.likeOrDislike = 0
-            privateFeed.save()
-            return Response({'detail': 'private feed like/dislike save success'}, status=status.HTTP_200_OK)
-        else:
+        user = self.request.user
+
+        if user != privateFeed.user:
             return Response({'detail': 'not authorized'}, status=status.HTTP_400_BAD_REQUEST)
+
+        likeOrDislike = request.data["likeOrDislike"]
+        originLike = privateFeed.likeOrDislike
+
+        # 사용자 관심사 키워드 리스트 업데이트
+        if likeOrDislike == 1 and originLike != 1:  # 좋아요 또는 싫어요 -> 좋아요
+            if privateFeed.title not in user.interest_keywords:
+                if len(user.interest_keywords) >= 15:
+                    user.interest_keywords.pop(0)  # 가장 오래된 항목 제거
+                user.interest_keywords.append(privateFeed.title)
+        elif originLike == 1:  # 좋아요 -> 싫어요 또는 좋아요 취소
+            if privateFeed.title in user.interest_keywords:
+                user.interest_keywords.remove(privateFeed.title)
+        user.save()
+
+        # privateFeed 좋아요 싫어요 업데이트
+        if likeOrDislike != originLike:
+            privateFeed.likeOrDislike = likeOrDislike
+        else:  # 좋아요/싫어요 취소
+            privateFeed.likeOrDislike = 0
+        privateFeed.save()
+
+        return Response({'detail': 'private feed like/dislike save success'}, status=status.HTTP_200_OK)
 
 
 class PublicFeedLikeView(APIView):
@@ -78,28 +86,35 @@ class PublicFeedLikeView(APIView):
 
     def post(self, request, pk):
         publicFeed = PublicFeed.objects.get(pk=pk)
+        user = self.request.user
         likeOrDislike = request.data["likeOrDislike"]
-        if self.request.user in publicFeed.liked_user.all():  # 이미 좋아요
-            if likeOrDislike == 1:  # 좋아요 취소
-                publicFeed.liked_user.remove(self.request.user)
-                publicFeed.save()
-            elif likeOrDislike == -1:  # 싫어요
-                publicFeed.disliked_user.add(self.request.user)
-                publicFeed.liked_user.remove(self.request.user)
-                publicFeed.save()
-        elif self.request.user in publicFeed.disliked_user.all():  # 이미 싫어요
-            if likeOrDislike == 1:  # 좋아요
-                publicFeed.liked_user.add(self.request.user)
-                publicFeed.disliked_user.remove(self.request.user)
-                publicFeed.save()
-            elif likeOrDislike == -1:  # 싫어요 취소
-                publicFeed.disliked_user.remove(self.request.user)
-                publicFeed.save()
-        else:  # 아무것도 안누른상태
-            if likeOrDislike == 1:  # 좋아요
-                publicFeed.liked_user.add(self.request.user)
-                publicFeed.save()
-            elif likeOrDislike == -1:  # 싫어요
-                publicFeed.disliked_user.add(self.request.user)
-                publicFeed.save()
+
+        alreadyLiked = user in publicFeed.liked_user.all()
+        alreadyDisliked = user in publicFeed.disliked_user.all()
+
+        if likeOrDislike == 1:  # 좋아요 관련 처리
+            if alreadyLiked:
+                publicFeed.liked_user.remove(user)  # 좋아요 취소
+                if publicFeed.title in user.interest_keywords:
+                    user.interest_keywords.remove(publicFeed.title)
+            else:
+                if alreadyDisliked:
+                    publicFeed.disliked_user.remove(user)  # 싫어요 -> 좋아요 전환
+                if publicFeed.title not in user.interest_keywords:
+                    if len(user.interest_keywords) >= 15:
+                        user.interest_keywords.pop(0)  # 가장 오래된 항목 제거
+                    user.interest_keywords.append(publicFeed.title)
+                publicFeed.liked_user.add(user)  # 순수 좋아요 클릭
+        elif likeOrDislike == -1:  # 싫어요 관련 처리
+            if alreadyDisliked:
+                publicFeed.disliked_user.remove(user)  # 싫어요 취소
+            else:
+                if alreadyLiked:
+                    publicFeed.liked_user.remove(user)  # 좋아요 -> 싫어요 전환
+                    if publicFeed.title in user.interest_keywords:
+                        user.interest_keywords.remove(publicFeed.title)
+                publicFeed.disliked_user.add(user)  # 순수 싫어요 클릭
+
+        user.save()
+        publicFeed.save()
         return Response({'detail': 'public feed like/dislike save success'}, status=status.HTTP_200_OK)
