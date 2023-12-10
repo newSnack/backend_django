9e381adf165c897da0577d04983a07cd5e963439
hdsung98 <hdsung98@naver.com>
2023-12-10 15:14:06 +0900

    chore: 안쓰는 news_crawling 파일들 주석처리

--- news_crawling/serializers.py
+++ news_crawling/serializers.py
@@ -1,20 +1,20 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
-from .models import News
-
-User = get_user_model()
-
-
-class NewsSerializer(serializers.ModelSerializer):
-    pub_date = serializers.SerializerMethodField()
-    user_id = serializers.SerializerMethodField()
-
-    class Meta:
-        model = News
-        fields = ['title', 'description', 'org_link', 'link', 'pub_date', 'content', 'user_id']
-
-    def get_pub_date(self, obj):
-        return obj.pub_date.strftime('%Y-%m-%d %H:%M:%S') if obj.pub_date else None
-
-    def get_user_id(self, obj):
-        return obj.user.id if obj.user else None
+# from django.contrib.auth import get_user_model
+# from rest_framework import serializers
+# from .models import News
+#
+# User = get_user_model()
+#
+#
+# class NewsSerializer(serializers.ModelSerializer):
+#     pub_date = serializers.SerializerMethodField()
+#     user_id = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = News
+#         fields = ['title', 'description', 'org_link', 'link', 'pub_date', 'content', 'user_id']
+#
+#     def get_pub_date(self, obj):
+#         return obj.pub_date.strftime('%Y-%m-%d %H:%M:%S') if obj.pub_date else None
+#
+#     def get_user_id(self, obj):
+#         return obj.user.id if obj.user else None
